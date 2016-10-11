44c2cd091bf31c8dcd1f1a8769f65f9dc55e92c3
FrogWu <Frog.Wu@software.dell.com>
2016-10-11 14:39:41 +0800

    create demo project

---
+++ mvc5demo/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using mvc5demo.Infrastructure;
+
+namespace mvc5demo.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ mvc5demo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace mvc5demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
